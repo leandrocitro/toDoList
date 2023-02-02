28f383f738535382aa457de8bb545b69d318c7c9
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2023-02-02 03:04:52 -0300

    estilizando tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,7 +4,7 @@ import style from './task.module.css'
 export function Task() {
     return (
         <body className={style.body}> 
-        <textarea>Tasks</textarea>
+        <textarea id="tasks" cols={60} rows={2}></textarea>
         <button>Add</button>
         </body>
     )
